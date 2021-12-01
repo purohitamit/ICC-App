f4b75ef02f5fe30d13e820c8a7d7ef07db941a73
amitpurohit <purohit_amit1000@rediff.com>
2021-12-01 22:27:51 +0000

    adding changes

--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -8,7 +8,7 @@ backend_host = "icc-app_backend:5000"
 @app.route('/home')
 def home():
     all_countries = requests.get(f"http://{backend_host}/read/allcountries").json()
-    return render_template('index.html', title = "Home", all_countries=all_countries["countries"])
+    return render_template('index.html', title = "Home", all_countries=all_countries["all_countries"])
 
 
 @app.route('/add/country', methods= ['GET', 'POST'])
