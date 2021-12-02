e8a5f5643dc22c26f6358335010fb1eea603f7a5
amitpurohit <purohit_amit1000@rediff.com>
2021-12-02 19:44:11 +0000

    adding changes

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -16,15 +16,15 @@ pip3 install -r backend/requirements.txt
 mkdir test_reports
 # run pytest frontend
 
-python3 -m pytest/frontend \
+python3 -m pytest frontend \
     --cov=frontend/application \
     --cov-report term-missing \
     --cov-report xml:test_reports/frontend_coverage.xml \
     --junitxml=test_reports/frontend_junit_report.xml
 
 # run pytest backend
-python3 -m pytest/backend \
---cov=frontend/application \
+python3 -m pytest backend \
+    --cov=backend/application \
     --cov-report term-missing \
     --cov-report xml:test_reports/backend_coverage.xml \
     --junitxml=test_reports/backend_junit_report.xml
